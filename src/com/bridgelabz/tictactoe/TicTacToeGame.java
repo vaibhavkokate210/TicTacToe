4ee68e8a1ab306ca8c9f33aeb15cc3bb3a8e8710
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-28 11:56:53 +0530

    [Vaibhav] added computer and user player select symbol

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame 
 {
+	static String computer;
+	static String user;
 	public static char[] board()
 	{
 		char ch[]=new char[10];
@@ -11,9 +15,22 @@ public class TicTacToeGame
 	    }
 	   return ch;
 	 }
+	public static void chooseSymbol()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter computer player X or O :");
+		computer=sc.nextLine();
+		System.out.println(computer);
+		if(computer.equals("X"))
+			user="O";
+		else
+			user="X";
+	}
 	public static void main(String[] args) 
 	{
 		char board[]=board();
+		chooseSymbol();
+		System.out.println("Computer = "+computer +" User = "+user);
 	}
 
 }
